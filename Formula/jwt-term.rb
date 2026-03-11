f0694ef000f98c92fea787336a91272fd3f652e4
Felipe Pires Morandini <felipepiresmorandini@gmail.com>
2026-03-11 00:47:36 +0000

    jwt-term: update to v1.0.1

--- Formula/jwt-term.rb
+++ Formula/jwt-term.rb
@@ -1,26 +1,26 @@
 class JwtTerm < Formula
   desc "A secure, offline-first CLI for inspecting, validating, and manipulating JWTs"
   homepage "https://github.com/felipemorandini/jwt-term"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/felipemorandini/jwt-term/releases/download/v#{version}/jwt-term-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "e375008ea128b336497643e89a391ebf51248b48593fde246bbc666bbf7dae4d"
+      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.0.1/jwt-term-aarch64-apple-darwin.tar.gz"
+      sha256 "d24dab7e861249aa20a1e7a800bfa83525f52e5293f0420767fa1caf8acbb698"
     else
-      url "https://github.com/felipemorandini/jwt-term/releases/download/v#{version}/jwt-term-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "3bc60bac0b0f6687e11e7d1004b295977f332669cfc5dfb7861fbe6433fed91c"
+      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.0.1/jwt-term-x86_64-apple-darwin.tar.gz"
+      sha256 "e5d03d11ad6b1a8518819c64111afeb9266436ce691812e7333f3d0c3fa52ddc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/felipemorandini/jwt-term/releases/download/v#{version}/jwt-term-v#{version}-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "42da7a882262d5fc249e958772a04ed87c21c1c180ccf33a9660910fa55fa5c9"
+      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.0.1/jwt-term-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "884bee4ec4df3fe210441968cbadbfe55d7313518308ad3ab1a2c3dc4d06c9ce"
     else
-      url "https://github.com/felipemorandini/jwt-term/releases/download/v#{version}/jwt-term-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "63b680c107a01eb43bbbfa32b6f52090c9f9dd4ab157fa1e2e09a11ea2280213"
+      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.0.1/jwt-term-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "129e738e06a45d669a0f2db1a197b8095234cb0aa9844d6457e2c655ab91381e"
     end
   end
 
