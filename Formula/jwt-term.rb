59a76509d6ce4141c49936e44e14c5f3257c8040
Felipe Pires Morandini <felipepiresmorandini@gmail.com>
2026-03-18 19:44:53 +0000

    jwt-term: update to v1.1.1

--- Formula/jwt-term.rb
+++ Formula/jwt-term.rb
@@ -7,20 +7,20 @@ class JwtTerm < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.1/jwt-term-aarch64-apple-darwin.tar.gz"
-      sha256 "942a2998af0255a861607b8cd87d8b58eda8b2ba0e3a9d5279ac826183db5ffb"
+      sha256 "ecdd3c1f602604509834e03d97ea286de094393e332fafd6011c9c49f74718a9"
     else
       url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.1/jwt-term-x86_64-apple-darwin.tar.gz"
-      sha256 "c6e8c2d14424a4b6a021e3bbd0dd33bf1c03e988f7d8d692bed9c1a7327eacd8"
+      sha256 "7431102f8c2fea2611fc1d5d068177ee7eaea51397505abbf1e2eee2f6b4db95"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.1/jwt-term-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "dd63882924c9899e0c73344fa16236bb34568d482cd904ac2289083a835654c8"
+      sha256 "53bdaca099e2e28108de19d46098d2bc5acb65e1f046ac0b3537681f5bc703f3"
     else
       url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.1/jwt-term-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "d97e01804ff5dfd7a8a74839a15570211ad0c11f9f38541de6085ba86af23bfa"
+      sha256 "c64d9bf71bfcf05d8c81423aa76f3b7f2970185cd4dfe28ade87e5d7cff671f8"
     end
   end
 
