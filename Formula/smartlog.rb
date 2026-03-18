ae2ab7cca6639efc638431c0abb31ad6ba0fd3a9
Felipe Pires Morandini <felipepiresmorandini@gmail.com>
2026-03-18 19:59:36 +0000

    smartlog: update to v0.6.1

--- Formula/smartlog.rb
+++ Formula/smartlog.rb
@@ -1,26 +1,26 @@
 class Smartlog < Formula
   desc "A high-performance TUI for log tailing with JSON auto-detection and real-time filtering"
   homepage "https://github.com/felipemorandini/smartlog"
-  version "0.6.0"
+  version "0.6.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.0/smartlog-aarch64-apple-darwin.tar.gz"
-      sha256 "7975a675d31c0f9ae8a3ddf2dcb196f8131c5e78309d7b7e2eeeee08ef0c40f2"
+      url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.1/smartlog-aarch64-apple-darwin.tar.gz"
+      sha256 "7c2644e0b55deaa000f9f2eeef72b6a6b2fbdfd44917c78fd325871fadf9b92b"
     else
-      url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.0/smartlog-x86_64-apple-darwin.tar.gz"
-      sha256 "c9a5e90cc9897b01d266858451e0ba67d795e776b61e47615fcf0c8e53f20578"
+      url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.1/smartlog-x86_64-apple-darwin.tar.gz"
+      sha256 "aae7d9ed68f752f78b7fa4e868396737944b1804924a322a0850313d6d43d8f5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.0/smartlog-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "fc017928fcca929f7e398d67098b8629c0c58478b43ce8010d303ee3aaddee43"
+      url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.1/smartlog-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "92f7986f101d3ea92065dd7ee217993f98e23199965a262ffac68dfacd5315b5"
     else
-      url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.0/smartlog-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "9eecf0a8b5a653f9caf723baa7116db21b66314bbc30e93798d1322a8559498f"
+      url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.1/smartlog-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "2bc49e132b573840d600884ca009bb58249ba6a44cf5eebdebc001223d4fa2bd"
     end
   end
 
