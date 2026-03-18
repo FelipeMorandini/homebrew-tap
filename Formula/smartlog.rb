a25fd36c293d9dafbcd1c81ed1a511e3de898f87
Felipe Pires Morandini <felipepiresmorandini@gmail.com>
2026-03-18 17:56:35 +0000

    smartlog: update to v0.6.0

--- Formula/smartlog.rb
+++ Formula/smartlog.rb
@@ -7,20 +7,20 @@ class Smartlog < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.0/smartlog-aarch64-apple-darwin.tar.gz"
-      sha256 "c158ec963ea96cc1a22dce75bae133a499ba1bee7d29345db24f9c87c7245cb2"
+      sha256 "7975a675d31c0f9ae8a3ddf2dcb196f8131c5e78309d7b7e2eeeee08ef0c40f2"
     else
       url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.0/smartlog-x86_64-apple-darwin.tar.gz"
-      sha256 "d64f7fdaac51b59bd04d235ccaa0024b6fbb680ed214a1a37a77a46ca7d8780a"
+      sha256 "c9a5e90cc9897b01d266858451e0ba67d795e776b61e47615fcf0c8e53f20578"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.0/smartlog-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "e764831ce192deac25d4f5272720036bb895faf8bad0f3d2925495851f750e4f"
+      sha256 "fc017928fcca929f7e398d67098b8629c0c58478b43ce8010d303ee3aaddee43"
     else
       url "https://github.com/felipemorandini/smartlog/releases/download/v0.6.0/smartlog-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "02d31e9244d38e59e73f055c86c0e33aefd4bd6557e90a8142c98ab4f361a920"
+      sha256 "9eecf0a8b5a653f9caf723baa7116db21b66314bbc30e93798d1322a8559498f"
     end
   end
 
