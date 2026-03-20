0293decafa7d5040f4bdfa9dc74b688c727d1423
Felipe Pires Morandini <felipepiresmorandini@gmail.com>
2026-03-20 15:25:38 +0000

    jwt-term: update to v1.1.4

--- Formula/jwt-term.rb
+++ Formula/jwt-term.rb
@@ -1,26 +1,26 @@
 class JwtTerm < Formula
   desc "A secure, offline-first CLI for inspecting, validating, and manipulating JWTs"
   homepage "https://github.com/felipemorandini/jwt-term"
-  version "1.1.3"
+  version "1.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.3/jwt-term-aarch64-apple-darwin.tar.gz"
-      sha256 "efb9ef5ce24227338a9c7e1a9c60d12726f5ac1f4b60761b91ceac2094b0f43e"
+      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.4/jwt-term-aarch64-apple-darwin.tar.gz"
+      sha256 "602dc2f8ef530d6be90d3fb208b11a8c7ebebb26ac423454b8645394d68643bb"
     else
-      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.3/jwt-term-x86_64-apple-darwin.tar.gz"
-      sha256 "2b4520ae77eb355cffe0a60c0cecae17ca6fda0b59fa114829ddfa779ec8ba72"
+      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.4/jwt-term-x86_64-apple-darwin.tar.gz"
+      sha256 "89f112135e078fcba49afb4358435d1f6b628c70acb8299b08eda031a15885be"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.3/jwt-term-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "96e7b6921c1c9c4b4672649dd1be1173b4ae5d0305eeb09db27f1ab9ed3c0770"
+      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.4/jwt-term-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "53b0cee4df90f89efdf356dbee4c044c8ae8a0d781c79c86fbefd409c2f904a8"
     else
-      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.3/jwt-term-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "57287d92d14fdadb2b0430727691138a715daee99eda134e55dd39dc37af6122"
+      url "https://github.com/felipemorandini/jwt-term/releases/download/v1.1.4/jwt-term-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "016dde36fb480d8333d2355efc3881e3597249f0edb530cd41a6b57b75a42da9"
     end
   end
 
